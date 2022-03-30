ad986dc1f87b24ffa70ddf6ebd1c7913edc1aa30
Fernando Martinotti <fermartinotti@gmail.com>
2022-03-30 10:25:18 -0300

    startup firestore

--- ViajerosApp/ViajerosAppApp.swift
+++ ViajerosApp/ViajerosAppApp.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ViajerosAppApp: App {
+    
+    @UIApplicationDelegateAdaptor (AppDelegate.self) var delegate
+    
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
